dff44ac89e9166dbaefbc6e07f5e8960e32d1c6f
vladimirmac <vladimirbodrin89@gmail.com>
2023-04-04 03:16:52 +0300

    added update_password

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
 from django.urls import path
+from rest_framework import routers
 
+from core.views import LoginView
+from core.views import ProfileView
 from core.views import SingUpView
+from core.views import UpdatePassword
 
 urlpatterns = [
-    path('signup', SingUpView.as_view(), name='signup')
+    path('signup', SingUpView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('update_password', UpdatePassword.as_view(), name='update_password'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,50 @@
 from __future__ import annotations
 
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth import logout
 from rest_framework.generics import CreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.generics import UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+from core.models import User
 from core.serializer import CreateUserSerializer
+from core.serializer import LoginSerializer
+from core.serializer import ProfileSerializer
+from core.serializer import UpdatePasswordSerializer
 
 
 class SingUpView(CreateAPIView):
     serializer_class = CreateUserSerializer
+
+
+class LoginView(CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def __create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(request=request, user=serializer.save())
+        return Response(serializer.data)
+
+
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def perform_destroy(self, instance):
+        logout(self.request)
+
+
+class UpdatePassword(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UpdatePasswordSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
